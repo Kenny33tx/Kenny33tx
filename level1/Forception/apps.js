71cc97f754a8b1f0e9f94a41fa48fc88811b4971
Kenny33tx <mckenneth.smith@gmail.com>
2023-01-01 16:01:58 -0600

    added new assignment.

--- level1/Forception/apps.js
+++ level1/Forception/apps.js
@@ -1,7 +1,8 @@
 var people = ["Jon", "Jacob", "Jingle", "Heimer", "Schmidt"]
-var alphabet = "abcdefghijklmnopqrstuvwxyz"
-
-for (var i = 0; i < people.length; i ++)
-for (var j = 0; j < alphabet; j++){
-    
+var alphabet = ["abcdefghijklmnopqrstuvwxyz"]
+var newArr = people.concat(alphabet)
+console.log(newArr)
+for (var i = 0; i < newArr.length; i++)
+for ( var j = 0; j < newArr.length[1]; j++){
+    console.log(newArr[0],[1])
 }
